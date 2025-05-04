016e515c55b4ba8e9ca794434b179a43b1a76b03
Lars T Hansen <lars.t.hansen@gmail.com>
2025-05-04 20:44:38 +0200

    Tweak

--- example_test.go
+++ example_test.go
@@ -7,16 +7,16 @@ import (
 	"github.com/lars-t-hansen/ini"
 )
 
-func ExampleParser_Parse() {
+func Example() {
 	p := ini.NewParser()
 	p.CommentChar = ';'
 
-	gSect := p.AddSection("global")
-	verbose := gSect.AddBool("verbose")
+	sGlobal := p.AddSection("global")
+	globalVerbose := sGlobal.AddBool("verbose")
 
-	gUser := p.AddSection("user")
-	userName := gUser.AddString("name")
-	userLevel := gUser.AddUint64("level")
+	sUser := p.AddSection("user")
+	userName := sUser.AddString("name")
+	userLevel := sUser.AddUint64("level")
 
 	store, err := p.Parse(strings.NewReader(`
 ; hi there
@@ -30,7 +30,7 @@ level= 37
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("global.verbose = %v\n", verbose.BoolVal(store))
+	fmt.Printf("global.verbose = %v\n", globalVerbose.BoolVal(store))
 	fmt.Printf("user.name = %s\n", userName.StringVal(store))
 	fmt.Printf("user.level = %d\n", userLevel.Uint64Val(store))
 	// Output:
